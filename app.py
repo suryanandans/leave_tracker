ab6e335c2579917ac921111056498cc6a986681d
suryanandans <suryanandan.sajan@gmail.com>
2025-07-30 15:54:29 +0530

    Implemented Login Page,Registration and authenticated login

--- app.py
+++ app.py
@@ -1,20 +1,39 @@
 
 from datetime import datetime
+from flask_bcrypt import Bcrypt
 import os
 from flask import Flask, request, jsonify, render_template
+from flask import session, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
 import io
 import csv
+from sqlalchemy import or_
 
 app = Flask(__name__)
+app.secret_key = 'leave_tracker_secret_key'
+bcrypt = Bcrypt(app)
 
 # Global list to temporarily store uploaded leave records
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost/leave_tracker'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+class AdminUser(db.Model):
+    __tablename__ = 'admin_users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
 
 class LeaveRecord(db.Model):
     __tablename__ = 'leave_records'
@@ -27,8 +46,67 @@ class LeaveRecord(db.Model):
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('login.html')
+
+
+@app.route('/dashboard')
+def dashboard():
+    if 'admin' not in session:
+        flash("Login to continue")
+        return redirect(url_for('login'))
+    else:
+        return render_template('index.html')
+    
+
+
+@app.route('/register',methods=['POST'])
+def register():
+    username=request.form.get('reg_username')
+    email=request.form.get('reg_email')
+    password=request.form.get('reg_password')
+    confirm_password=request.form.get('reg_confirm_password')
+
+    if not username or not email or not password:
+        flash("Kindly fill all the fields for completing registration")
+        return redirect(url_for('login'))
+    if password!=confirm_password:
+        flash("Password donot match!")
+        return redirect(url_for('login'))
+    existing = AdminUser.query.filter((AdminUser.username==username ) | (AdminUser.email==email)).first()
+    if existing:
+        flash("Username or Email Aldready Exists")
+        return redirect(url_for('login'))
+    new_admin=AdminUser(username=username, email=email)
+    new_admin.set_password(password)
+    db.session.add(new_admin)
+    db.session.commit()
+    flash("Registration Successfull! Login with given Credentials")
+    return redirect(url_for('login'))
+    
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+
+        username=request.form.get('login_username')
+        password=request.form.get('login_password')
+
+        print("Login Attempt → Username:", username, "| Password:", password)
+
+        admin=AdminUser.query.filter( or_(
+            AdminUser.username==username,
+            AdminUser.email==username
+        )).first()
+        print("Admin Found:", admin)
+        if admin and admin.check_password(password):
+            session['admin'] = admin.username
+            flash("Login Succesfull")
+            return redirect(url_for('dashboard'))
+        else:
+            flash("Incorrect credentials!")
+            return redirect(url_for('login'))
+    return render_template('login.html')
+    
 
 @app.route('/all_leaves', methods=['GET'])
 def all_leaves():
@@ -53,6 +131,9 @@ from sqlalchemy.exc import IntegrityError
 @app.route('/upload_csv', methods=['POST'])
 def upload_csv():
     file = request.files.get('file')
+    if 'admin' not in session:
+        flash("Session Expired")
+        return redirect(url_for('login'))
 
     if not file:
         return jsonify({'message': 'No file uploaded'}), 400
@@ -143,6 +224,12 @@ def get_leaves():
             'leave_type': r.leave_type
         } for r in results
     ])
+
+@app.route('/logout')
+def logout():
+    session.pop('admin',None)
+    flash("You have Loggged out Succesfully")
+    return redirect(url_for('login'))
 
 
 
